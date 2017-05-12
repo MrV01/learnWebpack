8271393bc9aa336dd85b191da9b75fba20643a07
Vlad V <VladV454@gmail.com>
2017-05-12 15:18:46 -0400

    Webpack is controlled by config file. TODO: testing

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './app/src/js/app.js',    // Core concept ENTRY = where to start
+    output: {   // Core concept  Output = where to end
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/build'
+    },
+    // Module - Loaders ( per file basis)
+    module: {  //  The Jorney  =  What to do with each individual file
+        rules: [
+            {
+                test: /\.css$/,
+                use: [     //  Array of loaders [] with options] . It works in reverse order   css-loader first, style-loader last
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    //  Webpack Plugins  array  ( Apply to the bundle)  What you should to do with prepared code
+    //   before gererating the bundle
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            // ...
+        })
+    ]
+};
